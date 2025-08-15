f3bc92d0743472d3637ed8885657964742fe4c3d
Trung-ETW <vn01122003@gmail.com>
2025-08-15 13:16:09 +0700

    final push

--- build_files.sh
+++ build_files.sh
@@ -4,7 +4,13 @@ set -e
 
 echo "BUILD START"
 
+# Bước 1: Cài đặt các thư viện từ requirements.txt
 python3.9 -m pip install -r requirements.txt
+
+# Bước 2: Chạy migrate để tạo bảng trong database (Dòng quan trọng đã thiếu)
+python3.9 manage.py migrate
+
+# Bước 3: Thu thập các file tĩnh
 python3.9 manage.py collectstatic --noinput --clear
 
 echo "BUILD END"
