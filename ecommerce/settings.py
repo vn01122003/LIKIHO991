d242cd2f750fffdebee606486ea151764127dbf5
Trung-ETW <vn01122003@gmail.com>
2025-08-18 11:14:24 +0700

    last change

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#vw(03o=(9kbvg!&2d5i!2$_58x@_-3l4wujpow6(ym37jxnza'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['likiho-991.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
